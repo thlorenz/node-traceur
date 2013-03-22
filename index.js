467412b6952c5b82c14041c38bd8803229382d22
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-22 07:28:17 -0400

    file utils are not part of public api

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 exports.traceur          =  require('./src/node/traceur');
 exports.compiler         =  require('./src/node/compiler');
-exports.fileUtil         =  require('./src/node/file-util');
 exports.inlineAndCompile =  require('./src/node/inline-module').inlineAndCompile;
 exports.interpret        =  require('./src/node/interpreter');
