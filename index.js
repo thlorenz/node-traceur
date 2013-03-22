4b574156112a42c66f3d2d845ea2f0d5ec4da261
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-21 21:29:21 -0400

    exposing node.js related api

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.traceur          =  require('./src/node/traceur');
+exports.compiler         =  require('./src/node/compiler');
+exports.fileUtil         =  require('./src/node/file-util');
+exports.inlineAndCompile =  require('./src/node/inline-module').inlineAndCompile;
+exports.interpret        =  require('./src/node/interpreter');
