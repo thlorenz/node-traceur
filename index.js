811e73b1d93a911ed2c0913eafa967f641894b85
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-23 15:56:27 -0400

    adding runtime path

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ exports.traceur          =  require('./src/node/traceur');
 exports.compiler         =  require('./src/node/compiler');
 exports.inlineAndCompile =  require('./src/node/inline-module').inlineAndCompile;
 exports.interpret        =  require('./src/node/interpreter');
+
+exports.runtimePath      =  require.resolve('./src/runtime/runtime');
